2d6d0813b9cec744fa9fd2727ffe11579917fa6c
marcnunezs <marc.nunezs@duocuc.cl>
2024-11-04 11:53:37 -0300

    firebase

--- AppTallerMecanico/src/app/app.module.ts
+++ AppTallerMecanico/src/app/app.module.ts
@@ -8,11 +8,14 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from '../environments/environment';
+
 
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, AngularFireModule.initializeApp(environment.firebaseConfig),],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
--- AppTallerMecanico/src/environments/environment.ts
+++ AppTallerMecanico/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyAMWzKnIRmZArS4isuQ1sWvCJLQ0_r4dLA",
+    authDomain: "appserviexpress001.firebaseapp.com",
+    projectId: "appserviexpress001",
+    storageBucket: "appserviexpress001.firebasestorage.app",
+    messagingSenderId: "616554579098",
+    appId: "1:616554579098:web:b51c4a75e41cba2f2f0b23",
+    measurementId: "G-5V66TJFVR8"
+  }
+  
 };
 
 /*
